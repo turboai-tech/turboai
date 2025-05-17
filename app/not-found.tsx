84264edd064729fae7f1c9ac7ae07cf1bc7ef77d
Shaoxiao Xu <lancexu.hello@gmail.com>
2025-05-18 05:50:48 +0800

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
   const t = useTranslations('NotFound');
 
   return (
-    <div className="w-full mx-auto min-h-[calc(100vh-100px)] flex flex-col items-center justify-center gap-10">
+    <div className="w-full mx-auto min-h-[calc(100vh-500px)] flex flex-col items-center justify-center gap-10">
       <div className="flex flex-col items-center justify-center">
         <h1 className="text-2xl font-bold">{t('title')}</h1>
         <p className="text-sm text-center mt-2">{t('description')}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,47 @@ import { getTranslations } from 'next-intl/server';
 export default async function App() {
   const t = await getTranslations();
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-100px)]">
-      <div className="inline-block mt-10 items-center justify-center">
-        <span className={title()}>{t('values.title_1')}&nbsp;</span>
-        <span className={title({ color: 'violet' })}>
-          {t('values.title_2')}&nbsp;
-        </span>
-        <span className={title()}>{t('values.title_3')}</span>
-        <div className={subtitle({ class: 'mt-4' })}>
+    <div className="flex flex-col md:flex-row items-center justify-center min-h-[calc(100vh-500px)] px-4 md:px-8">
+      <div
+        className="
+          w-full md:flex-[1]
+          flex flex-col
+          items-center md:items-center
+          text-center md:text-center
+        ">
+        <div className="text-center">
+          <span className={title()}>{t('values.title_1')}&nbsp;</span>
+          <span className={title({ color: 'violet' })}>
+            {t('values.title_2')}&nbsp;
+          </span>
+          <span className={title()}>{t('values.title_3')}</span>
+        </div>
+        <div className={subtitle({ class: 'mt-4 text-center' })}>
           {t('values.subtitle')}
+        </div>
+      </div>
+      <div
+        className="
+          w-full md:flex-[2]
+          flex flex-col
+          items-center md:items-start
+          text-center md:text-left
+        ">
+        <div className="mb-6">
+          <div className="text-xl font-semibold mb-2">
+            {t('values.vision.title')}
+          </div>
+          <div className="text-base text-gray-600">
+            {t('values.vision.content')}
+          </div>
+        </div>
+        <div>
+          <div className="text-xl font-semibold mb-2">
+            {t('values.mission.title')}
+          </div>
+          <div className="text-base text-gray-600">
+            {t('values.mission.content')}
+          </div>
         </div>
       </div>
     </div>
